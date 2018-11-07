ab813e7707f85e5255c2122c37f483700e242f50
Patrik Bernhardsson <patrik.bernhardsson@hv.se>
2018-11-07 13:43:43 +0100

    http.get hämtar data och ngFor loopar och visar data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,12 +8,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'angulario-pushArray-localStorage';
+  personer: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void { // adding the lifecycle hook ngOnInit
-    this.http.get('/assets/data.json').subscribe(data => {
-      console.log(data); // using the HttpClient instance, http to call the API then subscribe to the data and display to console
+    this.http.get('/assets/data.json').subscribe(retur_data => {
+      console.log(retur_data); // using the HttpClient instance, http to call the API then subscribe to the data and display to console
+      this.personer = retur_data;
     });
   }
 
