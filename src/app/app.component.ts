cd87f8a0e93f0b89fe96a35174c861de7d0d87f4
Patrik Bernhardsson <patrik.bernhardsson@hv.se>
2018-11-07 13:25:07 +0100

    La till http.get mot json-fil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulario-pushArray-localStorage';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void { // adding the lifecycle hook ngOnInit
+    this.http.get('/assets/data.json').subscribe(data => {
+      console.log(data); // using the HttpClient instance, http to call the API then subscribe to the data and display to console
+    });
+  }
+
 }
