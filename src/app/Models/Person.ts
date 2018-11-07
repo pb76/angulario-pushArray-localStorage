cf49404b446e6a5fb2a2104086fc693090f2dc0f
Patrik Bernhardsson <patrik.bernhardsson@hv.se>
2018-11-07 14:27:19 +0100

    push till json-model

---
+++ src/app/Models/Person.ts
@@ -0,0 +1,6 @@
+export class Person {
+
+    namn: string;
+    stad: string;
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,24 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angulario-pushArray-localStorage';
   personer: any;
+  arrTemp: any;
 
   constructor(private http: HttpClient) {}
+
+  addPerson() {
+    console.log('addPerson()');
+    console.log('Namn: ' + this.namnet);
+    console.log('Stad: ' + this.staden);
+    this.arrTemp  = {'namn': this.namnet,	'ort' : this.staden};
+    this.personer.push(this.arrTemp);
+
+    // Rensar formuläret
+    // this.namnet = '';
+    // this.staden = '';
+  }
 
   ngOnInit(): void { // adding the lifecycle hook ngOnInit
     this.http.get('/assets/data.json').subscribe(retur_data => {
